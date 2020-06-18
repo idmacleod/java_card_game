c466dc69f82ca53834b42a9a0f2dc176adca70cd
idmacleod <idmacleod@gmail.com>
2020-06-18 14:20:29 +0100

    Added Deck class and first test

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Deck {
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<Card>();
+    }
+
+    public int countCards() {
+        return cards.size();
+    }
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+    private Deck deck;
+    private ArrayList<Card> cards;
+
+
+    @Before
+    public void before() {
+        deck = new Deck();
+    }
+
+    @Test
+    public void deckIsEmptyToStart() {
+        assertEquals(0, deck.countCards());
+    }
 }
