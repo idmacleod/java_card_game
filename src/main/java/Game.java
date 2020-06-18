b64fb7c7e9bf0326f2d6475c1a68e7074ebf0454
idmacleod <idmacleod@gmail.com>
2020-06-18 15:41:50 +0100

    Added deck to Game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class Game {
+    private ArrayList<Player> players;
+    private Deck deck;
+
+    public Game(ArrayList<Player> players) {
+        this.players = players;
+        this.deck = new Deck();
+        this.deck.populate();
+        this.deck.shuffleCards();
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public int countPlayers() {
+        return players.size();
+    }
+
+    public void dealCards() {
+
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameTest {
+    private Player player1;
+    private Player player2;
+    private Player player3;
+    private ArrayList<Player> players;
+    private Game game;
+
+    @Before
+    public void before() {
+        player1 = new Player("Dave");
+        player2 = new Player("Iain");
+        player3 = new Player("Kirsty");
+        players = new ArrayList<Player>();
+        Collections.addAll(players, player1, player2, player3);
+        game = new Game(players);
+    }
+
+    @Test
+    public void hasDeck() {
+        assertEquals(52, game.getDeck().countCards());
+    }
+
+    @Test
+    public void canCountPlayers() {
+        assertEquals(3, game.countPlayers());
+    }
 }
