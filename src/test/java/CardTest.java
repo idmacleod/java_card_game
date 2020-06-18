b3c65dfbd4159d5c53dc278cd3300b36b94d42d3
idmacleod <idmacleod@gmail.com>
2020-06-18 14:12:49 +0100

    Added simple Card methods

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CardTest {
+    private Card card;
+
+    @Before
+    public void before() {
+        card = new Card(SuitType.SPADES, RankType.JACK);
+    }
+
+    @Test
+    public void hasSuit() {
+        assertEquals(SuitType.SPADES, card.getSuit());
+    }
+
+    @Test
+    public void hasRank() {
+        assertEquals(RankType.JACK, card.getRank());
+    }
+
+    @Test
+    public void canGetCardValue() {
+        assertEquals(10, card.getCardValue());
+    }
 }
